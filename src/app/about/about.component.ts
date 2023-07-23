4f1390cea7da438c1c93b067eb4ff951defecfe8
behnamps <behnamrezaeips@gmail.com>
2023-07-23 17:33:43 +0430

    about inputs in angular

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-
+  @Input() 
+  parentData: string = '';
+  
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,5 @@ import { AfterViewInit, Component, Input } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  title = 'firstapp';
-  myp = 'ptag'
-  myForm = 'myFormCss'
-
-  FullName = 'behnam';
-  Email ='behnam@gmail.com ';
-  Phone = '091000022';
-
-  Toggle : boolean = false;
-
-
-  ShowMessage() : void {
-    this.Toggle = !this.Toggle;
-  }
-
-  ValidateForm(formValue : string) : void {
-    console.log(formValue);
-  }
+    insert = 'My owner is behnam rezaei';
 }
